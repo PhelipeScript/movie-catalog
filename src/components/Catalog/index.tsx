a22bb2c93a8482452e5b578ac1eaff33de107405
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-03-19 17:47:39 -0300

    display the number of movies and an empty message in case of 0 movies

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
 
 import moviesImported from '../../../public/videos.json';
 import { MovieCard } from "../MovieCard";
@@ -25,33 +25,63 @@ export function Catalog({ sort, searchQuery }: CatalogProps) {
   const movies: MovieProps[] = sortMovies(filteredMovies, sort);
 
   return (
-    <FlatList
-      style={styles.container}
-      numColumns={2}
-      data={movies}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item, index }) => (
-        <MovieCard 
-          style={{ marginRight: index % 2 === 0 ? 16 : 0 }}
-          key={item.id}  
-          title={item.title}
-          description={item.description}
-          poster={item.poster}
-          year={item.year}
-          rating={item.rating}   
-        />
-      )}
-      contentContainerStyle={{ paddingBottom: 160, gap: 32 }}
-      showsVerticalScrollIndicator={false}
-      showsHorizontalScrollIndicator={false}
-    />
+    <View style={styles.container}>
+      <Text style={styles.moviesAmount}>
+        Total: {movies.length} Filmes
+      </Text>
+
+      <FlatList
+        numColumns={2}
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => (
+          <MovieCard 
+            style={{ marginRight: index % 2 === 0 ? 16 : 0 }}
+            key={item.id}  
+            title={item.title}
+            description={item.description}
+            poster={item.poster}
+            year={item.year}
+            rating={item.rating}   
+          />
+        )}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyText}>
+            Nenhum filme encontrado
+          </Text>
+        )}
+        contentContainerStyle={movies.length === 0 ? styles.emptyContainer : { paddingBottom: 160, gap: 32 }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    boxShadow: '0px 0px 2px 0px rgba(0, 0, 0, 0.5)',
-    paddingVertical: 16,
     flex: 1,
+    gap: 16,
+  },
+
+  emptyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  emptyText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#333',
+  },
+
+  moviesAmount: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal: 6,
+    marginBottom: 16,
+    width: '100%',
+    boxShadow: '0px 2px 0px 0px rgba(0, 0, 0, 0.1)',
   },
 })
